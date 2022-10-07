c25468eec5279d507fa102aea8a78cf83f2a62ea
David Losert <davelosert@github.com>
2022-10-07 18:09:51 +0000

    fix: Next test for plugin

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 const core = require('@actions/core');
 
 const person = core.getInput('person', { required: true });
-console.log(`Super Warm Welcome ${person}!`);
+console.log(`Again Warm Welcome ${person}!`);
